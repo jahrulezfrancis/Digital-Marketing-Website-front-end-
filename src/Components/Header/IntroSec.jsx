7227fd2c30d777ee7b395d429573ba82a5ea8eb0
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-10 09:45:12 +0100

    fixed header image running out

--- src/Components/Header/IntroSec.jsx
+++ src/Components/Header/IntroSec.jsx
@@ -8,11 +8,8 @@ export default function WelcomeBox() {
     return (
         <>
             <Box>
-                <Box bgColor='#ffffff' width='20em'>
-
-                </Box>
                 <UnorderedList color='white' listStyleType='none'>
-                    <HStack justify='flex-end' align='center' padding='3.5em .625em' m={'0em 5em'} gap={10}>
+                    <HStack justify='flex-end' align='center' padding='3.5em .625em' m={'0em 5em'} gap={10} w='100%'>
                         <ListItem marginRight='auto'><Image src={HeaderLogo} /></ListItem>
                         <ListItem>Products</ListItem>
                         <ListItem>Service</ListItem>
@@ -20,23 +17,29 @@ export default function WelcomeBox() {
                         <Button color='#0A2640'>Login</Button>
                     </HStack>
                 </UnorderedList>
-                <Box m={'1em 5em'} p='.7em .625em'>
-                    <Heading as={'h4'} size='md' fontFamily='Manrope' fontSize='2em'
-                        color='#ffffff' lineHeight='1.5em' fontWeight='1em'
-                    >
-                        Save time by building <br /> fast with Boldo Template
-                    </Heading>
-                    <Text color={'white'} fontSize='sm' fontFamily={'sans-serif'} pt={'1.4em'}>
-                        Funding handshake buyer business-to-business metrics iPad partnership. <br />
-                        First mover advantage innovator success deployment non-disclosure.
-                    </Text>
-                    <Box gap={3} mt='1.5em'>
-                        <Button bgColor={'#65E4A3'} mr={'1em'} border='2px solid #69E6A6' h='3em' w='15.4em'
-                            borderRadius='3.5em' p='16px 56px'
-                        >
-                            Buy Template
-                        </Button>
-                        <Button w='11.6em' h='3em' borderRadius='3.5em' border={'2px solid #FFFFFF'} p='16px 56px'>Explore</Button>
+                <Box m={'1em 5em'} p='.7em .625em' display='flex'>
+                    <Box width='50%'>
+                        <Box alignItems='center' mt='5em'>
+                            <Heading as={'h4'} size='md' fontFamily='Manrope' fontSize='2em'
+                                color='#ffffff' lineHeight='1.5em' fontWeight='1em'
+                            >
+                                Save time by building <br /> fast with Boldo Template
+                            </Heading>
+                            <Text color={'white'} fontSize='sm' fontFamily={'sans-serif'} pt={'1.4em'} mt='1em'>
+                                Funding handshake buyer business-to-business metrics iPad partnership. <br />
+                                First mover advantage innovator success deployment non-disclosure.
+                            </Text>
+                        </Box>
+                        <Box gap={3} mt='1.5em'>
+                            <Button bgColor={'#65E4A3'} mr={'1em'} border='2px solid #69E6A6' h='3em' w='15.4em'
+                                borderRadius='3.5em' p='16px 56px'
+                            >
+                                Buy Template
+                            </Button>
+                            <Button w='11.6em' h='3em' borderRadius='3.5em' border={'2px solid #FFFFFF'} p='16px 56px'>Explore</Button>
+                        </Box>
+                    </Box>
+                    <Box width={'50%'}>
                         <SideImages />
                     </Box>
                 </Box>
--- src/Components/Header/SideImage.jsx
+++ src/Components/Header/SideImage.jsx
@@ -5,8 +5,8 @@ import { VStack, HStack, Image, Spacer } from '@chakra-ui/react';
 
 export default function SideImages() {
     return (
-        <VStack padding={'1em'} mt='1em' display='flex' justify='flex-end'>
-            <Image src={Sideone} w='30.813em' />
+        <VStack mt='1em' display='flex' justify='flex-end'>
+            <Image src={Sideone} w='30.813em' h='15em' />
             <HStack pt='.4em' gap={14}>
                 <Image src={Sidetwo} boxSize='10.036em' />
                 <Image src={Sidetwo} />
