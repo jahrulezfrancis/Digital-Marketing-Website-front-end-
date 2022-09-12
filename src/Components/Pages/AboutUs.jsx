f2809771f3c7d7c9f67f549d30e772753bd146d2
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-12 12:01:19 +0100

    removed package.json and added about us page

---
+++ src/Components/Pages/AboutUs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function AboutUs() {
+    return (
+        <Text>We dey do am for here</Text>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -8,6 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <ChakraProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>
 );
