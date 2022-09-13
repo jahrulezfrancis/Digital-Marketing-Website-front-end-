052cb26982b9b03df9e4aaa930cff3ce32b39507
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-13 23:51:30 +0100

    Added the post section

--- src/Components/Pages/BlogPage.jsx
+++ src/Components/Pages/BlogPage.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import { ListItem, HStack, Box, UnorderedList, Image, Button, } from "@chakra-ui/react";
+import {
+    ListItem, HStack, Box, UnorderedList, Image, Button,
+    Flex, Stack, VStack, Heading, Text, Avatar
+} from "@chakra-ui/react";
 import { NavLink } from 'react-router-dom';
 import HeaderLogo from "../../Images/Logo.png"
-import Navigation from '../Header/Navigation';
+import FirstOne from "../../Images/Job-seeker.png"
+import SecondOne from "../../Images/pers-near-trash.png"
+import ThirdOne from "../../Images/woman-on-couch.png"
 
-export default function BlogSection() {
+
+function CardTemplate(props) {
     return (
-        <Box>
-            <UnorderedList color='black' listStyleType='none'>
+        <Box h='30em'>
+            <Image src={props.image} />
+            <VStack align='start'>
+                <HStack align='start' pt='2em' pb='1em'>
+                    <Text fontSize='1em' fontFamily="Open Sans" fontWeight='700' color='#0A2640'>Category</Text>
+                    <Text fontSize='1em' fontFamily="Open Sans" fontWeight='400' color='#777777'>November 22, 2022</Text>
+                </HStack>
+                {props.text}
+                <HStack>
+                    <Flex gap='3' alignItems='flex-end' justify='flex-end' >
+                        <Avatar size='sm' name='Jahrulez Francis' src={FirstOne} />
+                        <Flex direction='column' p='.2em'>
+                            <Text>Longsaar Francis</Text> <br />
+                        </Flex>
+                    </Flex>
+                </HStack>
+            </VStack>
+        </Box>
+    )
+}
+
+function Navigation() {
+    return (
+        <Box bgColor='#0A2640'>
+            <UnorderedList color='white' listStyleType='none' height='10em'>
                 <HStack justify='flex-end' align='center' padding='3.5em .625em' mt='0em' ml='1em' gap={10}>
                     <ListItem marginRight='auto'><Image src={HeaderLogo} /></ListItem>
                     <NavLink to="/">
@@ -23,5 +52,54 @@ export default function BlogSection() {
                 </HStack>
             </UnorderedList>
         </Box>
+    )
+}
+
+export default function BlogSection() {
+    return (
+        <Box>
+            <Navigation />
+            <Box w="100%" h='27em'>
+                <Stack align='center'>
+                    <VStack pt='8em'>
+                        <Text fontFamily='Open Sans' fontSize='1.25em'>Our Blog</Text>
+                        <Heading textAlign='center' fontFamily='Manrope' fontWeight='400' lineHeight='1.5em'>
+                            Value proposition accelerator product <br />
+                            management venture
+                        </Heading>
+                    </VStack>
+                    <HStack gap='8'>
+                        <CardTemplate
+                            image={FirstOne}
+                            text={<Text fontSize='1.25em' fontFamily="Open Sans" fontWeight='400' lineHeight='2em'>
+                                Pitch termsheet backing <br />
+                                validation focus release
+                            </Text>}
+                        />
+                        <CardTemplate
+                            image={SecondOne}
+                            text={<Text fontSize='1.25em' fontFamily="Open Sans" fontWeight='400' lineHeight='2em'>
+                                Seed round direct mailing non- <br />
+                                disclosure agreement graphical <br />
+                                user interface rockstar.
+                            </Text>}
+                        />
+                        <CardTemplate
+                            image={ThirdOne}
+                            text={<Text fontSize='1.25em' fontFamily="Open Sans" fontWeight='400' lineHeight='2em'>
+                                Beta prototype sales iPad gen-z <br />
+                                marketingnetwork effects value <br />
+                                proposition
+                            </Text>}
+                        />
+                    </HStack>
+                    <Button fontFamily='Open Sans' borderRadius='3em' border='solid 1px #0A2640' p='1.3em'
+                        width='10em' fontSize='1.2em' fontWeight='700'>
+                        Load More
+                    </Button>
+                </Stack>
+            </Box>
+        </Box>
+
     )
 }
