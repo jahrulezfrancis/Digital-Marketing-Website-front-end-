2b8ce37b03dd450c3b2b3beeffcfb1f2f006817b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-13 23:03:09 +0100

    Added route to about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './Components/Header/Header';
+import BlogSection from './Components/Pages/BlogPage';
 import HomePage from './Components/Pages/Home';
 import AboutIndex from './Components/Pages/Index/AboutIndex';
 
@@ -14,6 +14,7 @@ class App extends Component {
           <Route path='/' element={<HomePage />} />
           <Route path='home' element={<HomePage />} />
           <Route path='/about-us' element={<AboutIndex />} />
+          <Route path='/blog' element={<BlogSection />} />
         </Routes>
       </div>
     );
