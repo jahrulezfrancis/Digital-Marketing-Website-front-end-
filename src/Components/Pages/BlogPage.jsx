b950970120f42ce44c4ffca726e83d2e688a6fed
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-13 23:03:42 +0100

    initial commit

---
+++ src/Components/Pages/BlogPage.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import Navigation from '../Header/Navigation';
+
+export default function BlogSection() {
+    return(
+        <Box>
+            <Navigation />
+        </Box>
+    )
+}
