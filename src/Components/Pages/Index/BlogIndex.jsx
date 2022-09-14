58b34041a531b219fcf8007113179b503c5419f3
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-14 08:53:37 +0100

    initial commit

---
+++ src/Components/Pages/Index/BlogIndex.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import { NewsLetter } from '../Blog';
+import BlogSection from '../BlogPage';
+
+
+export function BlogIndex() {
+    return (
+        <Box>
+            <BlogSection />
+        </Box>
+    )
+}
