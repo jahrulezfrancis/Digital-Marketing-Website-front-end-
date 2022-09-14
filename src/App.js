df1cf4bb95282151212e51e9599e0eca257f24be
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-14 08:52:57 +0100

    changed route to Blog page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import BlogSection from './Components/Pages/BlogPage';
 import HomePage from './Components/Pages/Home';
 import AboutIndex from './Components/Pages/Index/AboutIndex';
+import { BlogIndex } from './Components/Pages/Index/BlogIndex';
 
 class App extends Component {
   render() {
@@ -14,7 +15,7 @@ class App extends Component {
           <Route path='/' element={<HomePage />} />
           <Route path='home' element={<HomePage />} />
           <Route path='/about-us' element={<AboutIndex />} />
-          <Route path='/blog' element={<BlogSection />} />
+          <Route path='/blog' element={<BlogIndex />} />
         </Routes>
       </div>
     );
